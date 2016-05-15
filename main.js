5db7706c931ec867b3f1a5e4a8db68ebd917ed02
Jesper Dalsgaard <post@jdanet.dk>
2016-05-15 16:41:22 +0200

    ...

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
-var netPriceOfShoes = 399.20; // interger
-var VAT = 25;
-var calculatedVAT = (1 + VAT / 100);
-var grossPriceOfShoes = (netPriceOfShoes * calculatedVAT);
-var amount = 10;
+// FUNKTIONER
 
-function calc() {
-    document.getElementById("result").innerHTML = "You bought a pair of shoes for the price of " + grossPriceOfShoes + " DKK. " + "Buy " + amount + " for the price of " + (grossPriceOfShoes * amount) + " DKK";
-};
+function sortNumber(a,b) {
+    return a - b;
+}
 
-// document.getElementById("result").innerHTML = grossPriceOfShoes;
+function findMin(nyArray) {
+    var min = nyArray[0];
+    for (var i = 0; i < nyArray.length; i++) {
+        if(nyArray[i] < min) {
+            min = nyArray[i];
+        }
+    }
+    return min;
+}
 
-// alert("You bought a pair of shoes for the price of " + grossPriceOfShoes + " DKK. " + "Buy " + amount + " for the price of " + (grossPriceOfShoes * amount) + " DKK");
+function findSum(sumArray) {
+    var sum = sumArray[0];
+    for (var i = 0; i < sumArray.length; i++) {
+        sum = sum + sumArray[i];
+    }
+    return sum;
+}
+
+function findMax(maxArray) {
+    var max = maxArray[0];
+    for (var i = 0; i < maxArray.length; i++) {
+        if(maxArray[i] > max) {
+            max = maxArray[i];
+        }
+    }
+    return max;
+}
+
+function findAverage(mitArray) {
+    return (findSum(mitArray) / mitArray.length);
+}
+
+// SLUT FUNKTIONER ---------------------------------------
+
